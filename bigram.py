a097c98ecf6f8b49bd9d869d1455ba6081fb5b93
CLOUDAPPEAL <yunshao666666666@gmail.com>
2025-06-11 14:50:23 +0800

    add self attention module

--- bigram.py
+++ bigram.py
@@ -7,9 +7,10 @@ batch_size = 32 # how many independent sequences will we process in parallel?
 block_size = 8 # what is the maximum context length for predictions?
 max_iters = 3000
 eval_interval = 300
-learning_rate = 1e-2
+learning_rate = 1e-3
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 eval_iters = 200
+n_embd = 32 # the dimensionality of the character embedding vectors
 # ------------
 
 torch.manual_seed(1337)
@@ -57,18 +58,50 @@ def estimate_loss():
     model.train()
     return out
 
+class Head(nn.Module):
+    def __init__(self, head_size):
+        super().__init__()
+        self.key = nn.Linear(n_embd, head_size, bias=False)
+        self.query = nn.Linear(n_embd, head_size, bias=False)
+        self.value = nn.Linear(n_embd, head_size, bias=False)
+        self.register_buffer('tril', torch.tril(torch.ones(block_size, block_size))) # 属性名为tril的下三角矩阵
+
+        # self.dropout = nn.Dropout(0.1)
+
+    def forward(self, x):
+        B, T, C = x.shape
+        k = self.key(x)   # (B,T,C) @ (C,head_size) ---> (B,T,head_size)
+        q = self.query(x) # (B,T,C) @ (C,head_size) ---> (B,T,head_size)
+
+        wei =  q @ k.transpose(-2, -1) * C ** -0.5# (B,T,head_size) @ (B,head_size,T) ---> (B,T,T)
+        wei = wei.masked_fill(self.tril[:T,:T] == 0, float('-inf'))
+        wei = F.softmax(wei, dim=-1)
+        # wei = self.dropout(wei)
+
+        v = self.value(x) # (B,T,C) @ (C,head_size) ---> (B,T,head_size)
+        out = wei @ v # (B,T,T) @ (B,T,head_size) ---> (B,T,head_size)
+
+        return out
+
 # super simple bigram model
 class BigramLanguageModel(nn.Module):
 
-    def __init__(self, vocab_size):
+    def __init__(self):
         super().__init__()
         # each token directly reads off the logits for the next token from a lookup table
-        self.token_embedding_table = nn.Embedding(vocab_size, vocab_size)
+        self.token_embedding_table = nn.Embedding(vocab_size, n_embd)
+        self.position_embedding_table = nn.Embedding(block_size, n_embd)
+        self.sa_head = Head(n_embd)  # single head attention
+        self.lm_head = nn.Linear(n_embd, vocab_size)
 
     def forward(self, idx, targets=None):
-
+        B ,T = idx.shape # B is batch size, T is block size
         # idx and targets are both (B,T) tensor of integers
-        logits = self.token_embedding_table(idx) # (B,T,C)
+        tok_emb = self.token_embedding_table(idx) # (B,T,C)
+        pos_emb = self.position_embedding_table(torch.arange(T, device=device)) # (T,C)
+        x = tok_emb + pos_emb # (B,T,C)
+        x = self.sa_head(x)
+        logits = self.lm_head(x)
 
         if targets is None:
             loss = None
@@ -83,8 +116,9 @@ class BigramLanguageModel(nn.Module):
     def generate(self, idx, max_new_tokens):
         # idx is (B, T) array of indices in the current context
         for _ in range(max_new_tokens):
+            idx_cond = idx[:, -block_size:] # (B, T) -> (B, block_size)
             # get the predictions
-            logits, loss = self(idx)
+            logits, loss = self(idx_cond)
             # focus only on the last time step
             logits = logits[:, -1, :] # becomes (B, C)
             # apply softmax to get probabilities
@@ -95,7 +129,7 @@ class BigramLanguageModel(nn.Module):
             idx = torch.cat((idx, idx_next), dim=1) # (B, T+1)
         return idx
 
-model = BigramLanguageModel(vocab_size)
+model = BigramLanguageModel()
 m = model.to(device)
 
 # create a PyTorch optimizer
